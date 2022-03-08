72592f23d40a118a8082b6ef4b897d1a34a70223
Zuhal Polat <zuhal.polat@trendyol.com>
2022-03-09 00:53:12 +0300

    changed app tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I am on the feature branch!
+          Learn React
         </a>
       </header>
     </div>
